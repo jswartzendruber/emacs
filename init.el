513c5a63d3911ca5fda33ad1f02290b7e65d8f1f
Jason <jdswart2@svsu.edu>
2022-12-03 08:16:57 -0500

    Add eglot

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+(use-package company)
+(global-company-mode)
+(use-package yasnippet)
+(yas-global-mode 1)
+
 ;; Hide bars
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -13,8 +18,15 @@
 (global-unset-key (kbd "C-z")) ;; stop freezing on me
 (setq shell-file-name "bash")
 (setq shell-command-switch "-ic")
-(setq initial-scratch-message ";; Hack with purpose.\n\n")
+(setq initial-scratch-message "\n\n")
 (setq inhibit-startup-message t)
+(add-to-list 'auto-mode-alist '("\\.jl\\'" . java-mode))
+
+;; Eglot
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
 
 ;; Compilation buffer ansi terminal colors
 (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
@@ -51,11 +63,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("00b463c48742afe509ae7d1dcfce09471f7203e13a118f1256b208017a978b4e" default))
+ '(eglot-ignored-server-capabilities '(:hoverProvider))
  '(package-selected-packages
-   '(cherry-blossom-theme boron-theme clang-format multiple-cursors auctex magit rustic lsp-mode gruber-darker-theme)))
+   '(yasnippet-snippets yasnippet company cherry-blossom-theme boron-theme clang-format multiple-cursors auctex magit rustic lsp-mode gruber-darker-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
